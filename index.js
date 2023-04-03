dfcf777a20b3b30937a6b81d5b55ef8bf170945a
Enoch Danijel <90690698+Danijel-Enoch@users.noreply.github.com>
2023-04-03 20:54:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+const bodyparser=require('body-parser')
+app.use(bodyparser.json())
+const all_routers=require("./routes");
+
+app.get('/',(req,res)=>{
+  res.send('welcome to succoin')
+})
+app.use("/api",all_routers);
+//qnea0donBwOJIvnN
+
+
+const CONNECTION_PORT="mongodb+srv://admin:qnea0donBwOJIvnN@succoincluster.unzqjfp.mongodb.net/test"
+const PORT=process.env.PORT || 8090;
+
+mongoose
+  .connect(CONNECTION_PORT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
